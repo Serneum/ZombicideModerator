b51e491b2ff49555c23332c89d2dae7f7b031e6d
Chris Rees <crees@covermymeds.com>
2017-09-20 20:44:30 -0400

    Add set_id to aboms, crowz, archers

--- priv/repo/seeds/spawn/crowz.exs
+++ priv/repo/seeds/spawn/crowz.exs
@@ -4,18 +4,18 @@ defmodule MurderOfCrowz do
   def seed() do
     Repo.insert!(%Set{id: 3, name: "Murder of Crows"})
 
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
-    Repo.insert!(%Spawn{blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
+    Repo.insert!(%Spawn{set_id: 3, blue: "Crowz x2", yellow: "Crowz x3", orange: "Crowz x4", red: "Crowz x5"})
   end
 end
 
--- priv/repo/seeds/spawn/deadeye.exs
+++ priv/repo/seeds/spawn/deadeye.exs
@@ -4,11 +4,11 @@ defmodule DeadeyeWalkers do
   def seed() do
     Repo.insert!(%Set{id: 4, name: "Deadeye Walkers"})
 
-    Repo.insert!(%Spawn{blue: "Deadeye Walker x1", yellow: "Deadeye Walker x2", orange: "Deadeye Walker x3", red: "Deadeye Walker x4"})
-    Repo.insert!(%Spawn{blue: "Deadeye Walker x2", yellow: "Deadeye Walker x3", orange: "Deadeye Walker x4", red: "Deadeye Walker x5"})
-    Repo.insert!(%Spawn{blue: "Deadeye Walker x3", yellow: "Deadeye Walker x4", orange: "Deadeye Walker x5", red: "Deadeye Walker x6"})
-    Repo.insert!(%Spawn{blue: "Deadeye Walker x1", yellow: "Deadeye Walker x2", orange: "Deadeye Walker x3", red: "Deadeye Walker x4"})
-    Repo.insert!(%Spawn{blue: "Deadeye Walker x2", yellow: "Deadeye Walker x3", orange: "Deadeye Walker x4", red: "Deadeye Walker x5"})
-    Repo.insert!(%Spawn{blue: "Deadeye Walker x3", yellow: "Deadeye Walker x4", orange: "Deadeye Walker x5", red: "Deadeye Walker x6"})
+    Repo.insert!(%Spawn{set_id: 4, blue: "Deadeye Walker x1", yellow: "Deadeye Walker x2", orange: "Deadeye Walker x3", red: "Deadeye Walker x4"})
+    Repo.insert!(%Spawn{set_id: 4, blue: "Deadeye Walker x2", yellow: "Deadeye Walker x3", orange: "Deadeye Walker x4", red: "Deadeye Walker x5"})
+    Repo.insert!(%Spawn{set_id: 4, blue: "Deadeye Walker x3", yellow: "Deadeye Walker x4", orange: "Deadeye Walker x5", red: "Deadeye Walker x6"})
+    Repo.insert!(%Spawn{set_id: 4, blue: "Deadeye Walker x1", yellow: "Deadeye Walker x2", orange: "Deadeye Walker x3", red: "Deadeye Walker x4"})
+    Repo.insert!(%Spawn{set_id: 4, blue: "Deadeye Walker x2", yellow: "Deadeye Walker x3", orange: "Deadeye Walker x4", red: "Deadeye Walker x5"})
+    Repo.insert!(%Spawn{set_id: 4, blue: "Deadeye Walker x3", yellow: "Deadeye Walker x4", orange: "Deadeye Walker x5", red: "Deadeye Walker x6"})
   end
 end
