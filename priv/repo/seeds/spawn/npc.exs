3bf446fa6378e4fc14eefeab9e9029f42b451fe8
Chris Rees <crees@covermymeds.com>
2017-09-22 17:14:08 -0400

    Add NPC-2

--- priv/repo/seeds/spawn/npc.exs
+++ priv/repo/seeds/spawn/npc.exs
@@ -4,7 +4,7 @@ defmodule NPC do
 
     def seed() do
       Repo.insert!(%Set{id: 5, name: "NPC-1"})
-      # Repo.insert!(%Set{id: 6, name: "NPC-2"})
+      Repo.insert!(%Set{id: 6, name: "NPC-2"})
 
       Repo.insert!(%Spawn{set_id: 5, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
@@ -14,10 +14,23 @@ defmodule NPC do
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x2", orange: "NPC x2", red: "NPC x8"})
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x2", yellow: "NPC x3", orange: "NPC x4", red: "NPC x4"})
+      Repo.insert!(%Spawn{set_id: 5, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
+      Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x2", orange: "NPC x2", red: "NPC x8"})
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x2", yellow: "NPC x3", orange: "NPC x4", red: "NPC x4"})
-      Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
-      Repo.insert!(%Spawn{set_id: 5, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
+
+      Repo.insert!(%Spawn{set_id: 6, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x1", yellow: "NPC x2", orange: "NPC x2", red: "NPC x8"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x2", yellow: "NPC x3", orange: "NPC x4", red: "NPC x4"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x1", yellow: "NPC x2", orange: "NPC x2", red: "NPC x8"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x2", yellow: "NPC x3", orange: "NPC x4", red: "NPC x4"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x1", yellow: "NPC x2", orange: "NPC x2", red: "NPC x8"})
+      Repo.insert!(%Spawn{set_id: 6, blue: "NPC x2", yellow: "NPC x3", orange: "NPC x4", red: "NPC x4"})
     end
   end
 end
