85b8979be9cb66d6fab03b097436c6470934f5af
Chris Rees <crees@covermymeds.com>
2017-09-22 17:02:07 -0400

    Dirty hack to organize cards by type

--- priv/repo/seeds/spawn/npc.exs
+++ priv/repo/seeds/spawn/npc.exs
@@ -4,6 +4,7 @@ defmodule NPC do
 
     def seed() do
       Repo.insert!(%Set{id: 5, name: "NPC-1"})
+      # Repo.insert!(%Set{id: 6, name: "NPC-2"})
 
       Repo.insert!(%Spawn{set_id: 5, blue: "Nothing in sight!", yellow: "NPC x4", orange: "NPC x4", red: "NPC x5"})
       Repo.insert!(%Spawn{set_id: 5, blue: "NPC x1", yellow: "NPC x1", orange: "NPC x6", red: "NPC x7"})
